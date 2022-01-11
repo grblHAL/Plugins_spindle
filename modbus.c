ac6abf23442def4125e98b4c0c9b76420ac6f4cd
Terje Io <terjeio@users.noreply.github.com>
2022-01-11 19:13:39 +0100

    Added subscription to new core event to poll for responses during delays, fixes issue with spindle at speed check for some drivers.

--- modbus.c
+++ modbus.c
@@ -86,7 +86,7 @@ static uint8_t dir_port;
 #endif
 
 static driver_reset_ptr driver_reset;
-static on_execute_realtime_ptr on_execute_realtime;
+static on_execute_realtime_ptr on_execute_realtime, on_execute_delay;
 static on_report_options_ptr on_report_options;
 static nvs_address_t nvs_address;
 
@@ -136,13 +136,9 @@ static inline void add_message (queue_entry_t *packet, modbus_message_t *msg, co
     }
 }
 
-void modbus_poll (sys_state_t grbl_state)
+static void modbus_poll (void)
 {
     static uint32_t last_ms;
-
-    UNUSED(grbl_state);
-
-    on_execute_realtime(grbl_state);
 
     uint32_t ms = hal.get_elapsed_ticks();
 
@@ -251,6 +247,20 @@ void modbus_poll (sys_state_t grbl_state)
 
     last_ms = ms;
     spin_lock = false;
+}
+
+static void modbus_poll_realtime (sys_state_t grbl_state)
+{
+    on_execute_realtime(grbl_state);
+
+    modbus_poll();
+}
+
+static void modbus_poll_delay (sys_state_t grbl_state)
+{
+    on_execute_delay(grbl_state);
+
+    modbus_poll();
 }
 
 bool modbus_send (modbus_message_t *msg, const modbus_callbacks_t *callbacks, bool block)
@@ -429,7 +439,7 @@ static void onReportOptions (bool newopt)
     on_report_options(newopt);
 
     if(!newopt)
-        hal.stream.write("[PLUGIN:MODBUS v0.09]" ASCII_EOL);
+        hal.stream.write("[PLUGIN:MODBUS v0.10]" ASCII_EOL);
 }
 
 bool modbus_isup (void)
@@ -564,7 +574,10 @@ void modbus_init (void)
         hal.driver_reset = modbus_reset;
 
         on_execute_realtime = grbl.on_execute_realtime;
-        grbl.on_execute_realtime = modbus_poll;
+        grbl.on_execute_realtime = modbus_poll_realtime;
+
+        on_execute_delay = grbl.on_execute_delay;
+        grbl.on_execute_delay = modbus_poll_delay;
 
         on_report_options = grbl.on_report_options;
         grbl.on_report_options = onReportOptions;
